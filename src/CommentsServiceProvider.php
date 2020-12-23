520e368eb072e9c32a21129b723a3f0a1c973b29
Ignacio Hurtado <ignaciohurtado91@gmail.com>
2020-12-23 15:54:02 -0300

    Inicializo estructura básica

---
+++ src/CommentsServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Iehurtado\Comments;
+
+use Illuminate\Support\ServiceProvider;
+
+class CommentsServiceProvider extends ServiceProvider
+{
+    public function register() 
+    {
+        //
+    }
+    
+    public function boot()
+    {
+        //
+    }
+}
